23a1f6abbfca57bce579de336c309398305e8af3
DrTube <zhangchaofan9627@gmail.com>
2021-03-28 01:23:55 +0800

    add assignment 5

---
+++ assignments/assignment5/expressLL.c
@@ -0,0 +1,167 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 80 //The size of the array
+
+enum ExpType {OPT,OPERAND};
+
+typedef struct _listnode
+{
+    int  item;
+    enum ExpType type;
+	struct _listnode *next;
+} ListNode;
+
+typedef struct _linkedlist{
+   int size;
+   ListNode *head;
+} LinkedList;
+
+void insertNode(LinkedList *llPtr, int item, enum ExpType type);
+int deleteNode(LinkedList *llPtr);
+void removeAllNodes(LinkedList *llPtr);
+int isEmptyLinkedList (LinkedList ll);
+
+void expressionLL(char* infix, LinkedList *inExpLL);
+
+void printExpLL(LinkedList inExpQ, int seed);
+
+int main()
+{
+    char infix[SIZE];
+
+    printf("Enter an infix expression:\n");
+    gets(infix);
+
+    LinkedList inExpLL;
+    inExpLL.head = NULL;
+    inExpLL.size = 0;
+
+    expressionLL(infix, &inExpLL);
+
+    int seed;
+	printf("Enter a seed: \n");
+    scanf("%d",&seed);
+    printExpLL(inExpLL,seed);
+
+    removeAllNodes(&inExpLL);
+    return 0;
+}
+
+
+void expressionLL(char* infix, LinkedList *inExpLL)
+{
+    //Write your code here
+    //reverse the string first
+    int count, i;
+    count = 0;
+    char revInfix[50];
+	while(infix[count]!='\0')count++;
+	for(i=count-1;i>=0;i--)
+		revInfix[count-1-i] = infix[i];
+   	revInfix[count]='\0';
+
+
+    char *ptr = revInfix;
+    char numarr[20];
+    int index = 0;
+    int isoperandlast = 0;
+
+
+    while(*ptr!='\0'){
+        char c = *ptr;
+        enum ExpType mytype = (c=='+'||c=='-'||c=='*'||c=='/'||c=='('||c==')') ? OPT : OPERAND;
+
+        if(mytype == OPT){
+			if(index!=0){
+				numarr[index] = '\0';
+				char revnumarr[20];
+				int i, j;
+				j=index;
+
+				j--;
+				for(i = 0; i<index; i++){
+					revnumarr[i] = numarr[j];
+					j--;
+				}
+				revnumarr[index]='\0';
+				int myint = atoi(revnumarr);
+				insertNode(inExpLL, myint, OPERAND);
+			}
+			insertNode(inExpLL, c, mytype);
+			index = 0;
+			numarr[0]='\0';
+        }
+        else{
+			numarr[index] = c;
+			index++;
+        }
+        ptr++;
+    }
+
+    if(numarr[0]!='\0'){
+				numarr[index] = '\0';
+				char revnumarr[20];
+				int i, j;
+				j=index;
+
+				j--;
+				for(i = 0; i<index; i++){
+					revnumarr[i] = numarr[j];
+					j--;
+				}
+				revnumarr[index]='\0';
+				int myint = atoi(revnumarr);
+				insertNode(inExpLL, myint, OPERAND);
+    }
+}
+
+void printExpLL(LinkedList inExpLL, int seed)
+{
+    ListNode* temp = NULL;
+    temp = inExpLL.head;
+    while(temp!= NULL){
+        if(temp->type == OPERAND)
+            printf(" %d ",temp->item+ seed);
+        else
+            printf(" %c ",(char)(temp->item));
+        temp = temp->next;
+    }
+    printf("\n");
+}
+
+void insertNode(LinkedList *LLPtr, int item, enum ExpType type) {
+    ListNode *newNode;
+    newNode = malloc(sizeof(ListNode));
+    if(newNode==NULL) exit(0);
+
+    newNode->item = item;
+    newNode->type = type;
+    newNode->next = LLPtr->head;
+    LLPtr->head=newNode;
+    LLPtr->size++;
+}
+
+int deleteNode(LinkedList *LLPtr) {
+    if(LLPtr==NULL || LLPtr->size==0){ //Queue is empty or NULL pointer
+        return 0;
+    }
+    else{
+       ListNode *temp = LLPtr->head;
+       LLPtr->head = LLPtr->head->next;
+
+       free(temp);
+       LLPtr->size--;
+       return 1;
+    }
+}
+
+int isEmptyLinkedList (LinkedList ll) {
+    if(ll.size==0) return 1;
+    else return 0;
+}
+
+void removeAllNodes(LinkedList *LLPtr)
+{
+	while(deleteNode(LLPtr));
+}
